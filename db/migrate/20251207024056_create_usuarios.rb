a87402da6dbfb935cb93da76bbc99b679610ffa6
Bruno Tarquinio Silva <bruno.ts04@gmail.com>
2025-12-07 00:38:15 -0300

    update rb file

--- db/migrate/20251207024056_create_usuarios.rb
+++ db/migrate/20251207024056_create_usuarios.rb
@@ -1,18 +1,17 @@
-class CreateUsuarios < ActiveRecord::Migration[8.0] # ou [7.1] dependendo da sua versão
+class CreateUsuarios < ActiveRecord::Migration[8.0]
   def change
     create_table :usuarios do |t|
       t.string :login
       t.string :matricula
-      t.string :nome              # Adicionado (Tarefa #5)
+      t.string :nome              
       t.string :email
-      t.string :formacao          # Adicionado (Tarefa #5)
-      t.string :password_digest   # Mantivemos o padrão do Rails
-      t.boolean :eh_admin, default: false # Adicionado (Tarefa #5)
+      t.string :formacao         
+      t.string :password_digest  
+      t.boolean :eh_admin, default: false
 
       t.timestamps
     end
 
-    # Adicionando restrição de unicidade direto no Banco de Dados (Tarefa #5)
     add_index :usuarios, :login, unique: true
     add_index :usuarios, :matricula, unique: true
     add_index :usuarios, :email, unique: true
