31466244b59c6f6dd47aefa2a848a4525ec0dda4
PPeregrino <125605570+PPeregrino@users.noreply.github.com>
2025-12-07 00:53:17 -0300

    Create migration for turmas

--- db/migrate/20231026000001_create_turmas.rb
+++ db/migrate/20231026000001_create_turmas.rb
@@ -1,1 +1,11 @@
+class CreateTurmas < ActiveRecord::Migration[7.0]
+  def change
+    create_table :turmas do |t|
+      t.string :codigo
+      t.string :nome
+      t.string :semestre
 
+      t.timestamps
+    end
+  end
+end
