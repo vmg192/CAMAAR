82a563f2e1db2eab459bf537c480e6917748e90c
PPeregrino <125605570+PPeregrino@users.noreply.github.com>
2025-12-11 17:55:52 -0300

    teste rspec para model de "Turma" #85
    Refactor association tests for Turma model to use explicit expectations.

--- spec/models/turma_spec.rb
+++ spec/models/turma_spec.rb
@@ -1,14 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Turma, type: :model do
-  describe 'validations' do
-    it { should validate_presence_of(:codigo) }
-    it { should validate_presence_of(:nome) }
-    it { should validate_presence_of(:semestre) }
-  end
-
   describe 'associations' do
-    it { should have_many(:matricula_turmas) }
-    it { should have_many(:usuarios).through(:matricula_turmas) }
+    it 'has many matricula_turmas' do
+      association = described_class.reflect_on_association(:matricula_turmas)
+      expect(association.macro).to eq :has_many
+    end
+
+    it 'has many users through matricula_turmas' do
+      association = described_class.reflect_on_association(:users)
+      expect(association.macro).to eq :has_many
+      expect(association.options[:through]).to eq :matricula_turmas
+    end
   end
 end
