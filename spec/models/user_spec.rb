848a3b06211407d971a3c1667172109925babd22
PPeregrino <125605570+PPeregrino@users.noreply.github.com>
2025-12-11 17:55:20 -0300

    teste rspec para model de "User" #84

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,16 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'associations' do
-    it { should have_many(:matricula_turmas) }
-    it { should have_many(:turmas).through(:matricula_turmas) }
+    it 'has many matricula_turmas' do
+      association = described_class.reflect_on_association(:matricula_turmas)
+      expect(association.macro).to eq :has_many
+    end
+
+    it 'has many turmas through matricula_turmas' do
+      association = described_class.reflect_on_association(:turmas)
+      expect(association.macro).to eq :has_many
+      expect(association.options[:through]).to eq :matricula_turmas
+    end
   end
 
   describe 'validations' do
